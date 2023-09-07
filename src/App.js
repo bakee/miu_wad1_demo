606b803ec8136df45b8e9e2aefc936bc7be4fdc1
Abdullah <mail.bakee@gmail.com>
2023-09-07 16:53:26 -0500

    Add initial code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+function StudentRenderer({ count}) {
+  return (
+    <li className="item">
+
+      {/* {count} {count > 1 ? "students" : "student"} */}
+      {count} student{count > 1 && "s"}
+    </li>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StudentRenderer count={7}  />
+      <StudentRenderer count={1}  />
     </div>
   );
 }
